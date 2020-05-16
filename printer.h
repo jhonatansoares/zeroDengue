ccfe82324b604656a49867f3e56673df8b422dac
jhonatan <jhonatansoares62@gmail.com>
2020-05-16 14:19:36 -0300

    c5 excluindo main.exe adicionando printer.h

---
+++ printer.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "usuario.h"
+#include "telefone.h"
+#include "email.h"
+#include "endereco.h"
+#include "denuncia.h"
+#include "usuario.h"
+#include "data.h"
+#include "filaUsuario.h"
+#include "filaDenuncia.h"
+
+
+
+void printUsuario(Usuario *);
+void printTelefone(Telefone *);
+void printEmail(Email *);
+void printEndereco(Endereco *);
+void printDenuncia(Denuncia *);
+void printData(Data *);
+void printFilaUsuario(FilaUsuario *);
+void printFilaDenuncia(FilaDenuncia *);
+
+
